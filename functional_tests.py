1b8f8c0c6e3e30a860d2bcfd1519c21925a91cb6
qianqing118 <andyqq_118@163.com>
2017-09-18 23:24:10 +0800

    first commit

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
         pass
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
@@ -26,14 +31,14 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('1: Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        sleep(5)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('2: Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
 
