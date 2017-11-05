a4f5b657dcf8ba7b947b2dd5340f6e592e24643a
qianqing118 <andyqq_118@163.com>
2017-11-05 22:31:21 +0800

    first commit

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+from lists.models import Item
 
 def home_page(request):
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        # new_item_list = request.POST['item_text']
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'item': items})
+
+
+
+    #     Item.objects.create(text=new_item_list)
+    # else:
+    #     new_item_list = ''
+    # # item = Item()
+    # # item.text = request.POST.get('item_text', '')
+    # # item.save()
+    #
+    # return render(request, 'home.html', {
+    #     'new_item_text': new_item_list})
+    #
